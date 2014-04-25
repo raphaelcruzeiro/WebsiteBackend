bb1530d802a6054af95e40b75c40b35e5680d622
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2014-04-25 20:08:10 -0300

    Removed unused apps.

--- WebsiteBackend/settings/base.py
+++ WebsiteBackend/settings/base.py
@@ -135,10 +135,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'corsheaders',
     'south',
-    'compressor',
-    'debug_toolbar',
-
-    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'rest_framework',
